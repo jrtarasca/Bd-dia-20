91e040f477e88354d5a4e2918c897d4b9c3f2a7a
jrtarasca <166664327+jrtarasca@users.noreply.github.com>
2024-10-04 20:29:02 -0300

    Add files via upload

---
+++ insert.sql
@@ -0,0 +1,17 @@
+INSERT INTO Cliente (id, nome, data_nasc, altura, idade, email, vip)
+	VALUES(
+    1,
+    'José Roberto',
+    '2005-28-07',
+    1.74,
+    19,
+    'jose.tarasca@gmail.com',
+    true),
+    (2,
+    'Marcia Tarasca',
+    58,
+    '1967-10-27',
+    1.60,
+    'marcia.tarasca@gmail.cm',
+    true
+    );cliente_db
